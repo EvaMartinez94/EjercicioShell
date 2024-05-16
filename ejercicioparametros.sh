b7b336360759803dca7c118e5bba7347fad6a438
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-05-16 19:48:41 +0200

    Mejorar

--- ejercicioparametros.sh
+++ ejercicioparametros.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# Ejercicio 1:
+# -Pasar tres parámetros e imprime el que está en la posición 1 y 3
+
 echo $1 $3
+
+# Ejercicio 2:
+# -Completar la siguiente frase:
+# “En el fichero” <aquí va el nombre del fichero> “existen” <aquí va la cantidad de parámetros>
 
