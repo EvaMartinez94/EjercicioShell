a9a514aeef084bd47ed2d16fcabc71e827252012
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-05-16 19:42:42 +0200

    Mejorar

--- ejerciciovariables.sh
+++ ejerciciovariables.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+
+# #Ejercicio 1:
+# -Escribir el siguiente texto con variables:
+# “Mi nombre es” <nombre> “tengo” <edad> “años y vivo en” <ciudad>
+
 miNombre=Eva
 miEdad=29
 miCiudad=Sevilla
 
 echo "Mi nombre es $miNombre, tengo $miEdad años y vivo en $miCiudad"
 
-actividad="ir al cine"
-comida="las croquetas"
+# Ejercicio 2:
+# -Escribir el siguiente texto pero con inputs por consola:
+# “Mi actividad favorita es $actividad. Mi comida favorita es $comida”
+
+echo "¿Cuál es tu actividad favorita?"
+read actividad
+echo "¿Y tu comida favorita?"
+read comida
 
 echo "Mi actividad favorita es $actividad. Mi comida favorita son $comida"
