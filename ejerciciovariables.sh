3e5c9252f98d3314cbf7ca059a2e8c38b3463b3b
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-05-16 14:40:01 +0200

    ejerciciovariables

---
+++ ejerciciovariables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+miNombre=Eva
+miEdad=29
+miCiudad=Sevilla
+
+echo "Mi nombre es $miNombre, tengo $miEdad años y vivo en $miCiudad"
+
+actividad="ir al cine"
+comida="las croquetas"
+
+echo "Mi actividad favorita es $actividad. Mi comida favorita son $comida"
