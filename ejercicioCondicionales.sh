c7bd56fe75d2324a3432bb8e0ac45681545b24ad
EvaMartinez94 <evamartinezlopez94@hotmail.com>
2024-05-17 10:40:13 +0200

    Ej

---
+++ ejercicioCondicionales.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ejercicio 1:
+# Verificar que usuario esta actualmente ejecutando
+# Hacer un Script que verifique el usuario y en el caso que de que nos sea Root nos arroje un mensaje de error “No tiene acceso como root”
+
+# Para que te diga el id es $(id)
+
+echo $id
+
--- ejercicioparametros.sh
+++ ejercicioparametros.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # Ejercicio 1:
 # -Pasar tres parámetros e imprime el que está en la posición 1 y 3
-
 echo $1 $3
+echo "El parámetro 1 y 3 son $1 y $3"
 
 # Ejercicio 2:
-# -Completar la siguiente frase:
+# - Completar la siguiente frase:
 # “En el fichero” <aquí va el nombre del fichero> “existen” <aquí va la cantidad de parámetros>
 
+echo "En el fichero $0 existen $# parámetros"
+
+# $0 (Para que te de el nombre del fichero)
+# $# (Cantidad de parametros que recibe el script)
